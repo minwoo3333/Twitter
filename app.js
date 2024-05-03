08f8d74ef3893e786c59e453c42eb5eb5d5c47c8
Minwoo <jeonminwoo3333@gmail.com>
2024-05-03 09:54:45 +0900

    TOken

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+import tweetsRouter from './router/tweets.js'
+import authRouter from './router/auth.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use('/tweets', tweetsRouter);
+app.use('/auth', authRouter);``
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.listen(8080);
---
+++ controller/auth.js
@@ -0,0 +1,69 @@
+import * as authRepository from '../data/auth.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const secretKey = "abcd1234%^&*"
+
+const jwtExpiresInDays ='2d';
+const bcryptSaltRounds = 10;
+
+
+
+// async function makeToken(id){
+//     const token = jwt.sign({
+//         id: id,
+//         isAdmin: false
+//     }, secret, {expiresIn: '1h'})
+//     return token;
+// }
+
+function createJwtToken(id){
+    return jwt.sign({id},secretKey,{expiresIn: jwtExpiresInDays});
+
+}
+
+
+export async function signup(req, res, next){
+    const {username, password, name, email,url} = req.body;
+    const found = await authRepository.findByUsername(username);
+    if(found){
+        return res.status(409).json({message:`${username}이 이미 있습니다`});
+    }
+    const hashed = await bcrypt.hash(password, bcryptSaltRounds);
+    const userId = await authRepository.createUser({username, hashed, name, email,url});
+    const token = createJwtToken(userId);
+    res.status(201).json({token,username});
+    
+}
+
+
+export async function login(req, res, next){
+    const {username, password} = req.body;
+    // const user = await authRepository.login(username);
+    const user = await authRepository.findByUsername(username);
+    if(!user){
+        return res.status(401).json({message:`아이디를 찾을 수 없음`});
+    }
+    const isValidpassword = await bcrypt.compareSync(password,user.password);
+    if(!isValidpassword){
+        return res.status(401).json({message:`비밀번호가 틀렸음`});
+
+    }
+    const token = createJwtToken(user.id);
+    res.status(200).json({token,username});
+}
+
+// export async function verify(req, res, next){
+//     const token = req.header['Token'];
+//     if(token){
+//         res.status(200).json(token);
+//     }
+// }
+
+export async function me(req, res, next){
+    const user = await authRepository.findById(req.userId);
+    if(!user){
+        return res.status(404).json({message:`일치하는 사용자가 없음`})
+    }
+    res.status(200).json({token:req.token,username: user.username});
+}
