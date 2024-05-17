bef3b843e887256b4b97f66caa61432daa41dbc6
Minwoo <jeonminwoo3333@gmail.com>
2024-05-17 11:46:01 +0900

    socket 적용

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import tweetsRouter from "./router/tweets.js";
 import authRouter from "./router/auth.js";
 import { config } from "./config.js";
 import { connectDB } from "./db/database.js";
+import { initSocket } from "./connection/socket.js";
 
 
 const app = express();
@@ -18,10 +19,12 @@ app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
+
 // DB 연결 테스트
 
 connectDB().then((db)=> {
-    console.log('몽구스를 사용하여 몽고디비에 접속 성공!')
-    app.listen(config.host.port);
+    const server = app.listen(config.host.port);
+    initSocket(server);
+
 }).catch(console.error);
 
